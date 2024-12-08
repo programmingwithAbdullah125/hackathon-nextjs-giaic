f0144f19349c6d86078617f16dee9e56d33a0efa
Abdullah <abdullahateeq331@gmail.com>
2024-12-09 03:07:09 +0500

    some changes

--- my-app/src/components/ui/card.tsx
+++ my-app/src/components/ui/card.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -8,10 +7,8 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
+    className=
+      "rounded-lg border bg-card text-card-foreground shadow-sm"
     {...props}
   />
 ))
@@ -23,7 +20,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className="flex flex-col space-y-1.5 p-6"
     {...props}
   />
 ))
@@ -35,10 +32,8 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
+    className=
+      "text-2xl font-semibold leading-none tracking-tight"
     {...props}
   />
 ))
@@ -50,7 +45,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className="text-sm text-muted-foreground"
     {...props}
   />
 ))
@@ -60,7 +55,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className= "p-6 pt-0" {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -70,7 +65,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className="flex items-center p-6 pt-0"
     {...props}
   />
 ))
